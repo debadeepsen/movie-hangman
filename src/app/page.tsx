23147372b4c12c559835e6432de93bc515a7d119
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-09-29 15:50:59 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,176 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { ALPHABETS } from '@/lib/gameConstants'
+import React, { useState, useEffect } from 'react'
+import '@/style/word-game.css'
+import { topMovies } from '@/data/topMovies'
+import { Movie } from '@/lib/types'
+
+const WordGame = () => {
+  const [currentMovie, setCurrentMovie] = useState<Movie | null>(null)
+
+  const [ei, setEi] = useState(0)
+  const [currentGuess, setCurrentGuess] = useState<string[]>([])
+  const [tries, setTries] = useState(0)
+  const [progress, setProgress] = useState(0)
+
+  const alphabets: string[] = ALPHABETS.split('')
+
+  const currentMovieTitle = () => currentMovie?.title
+
+  // const progressPercent: number = Math.round(
+  //   (progress / currentWord.length) * 100
+  // )
+  // const degrees: number = Math.round((progress / currentWord.length) * 360)
+
+  const puzzleSolved = () =>
+    !!currentMovieTitle()?.replaceAll(' ', '').length &&
+    progress === currentMovieTitle()?.length
+
+  const triesWord = () => (tries == 1 ? 'try' : 'tries')
+
+  const reset = () => {
+    setCurrentMovie(null)
+    setEi(0)
+    setCurrentGuess([])
+    setTries(0)
+    setProgress(0)
+  }
+
+  const loadGame = () => {
+    reset()
+
+    const rnd: number = Math.floor(Math.random() * topMovies.length)
+    const movie = topMovies[rnd]
+
+    setCurrentMovie(movie)
+
+    console.log(movie)
+  }
+
+  const getGuessedLetter = (index: number): string => {
+    if (currentMovieTitle() === undefined) return ''
+
+    if (currentGuess.includes(currentMovieTitle()?.[index] ?? '******')) {
+      return currentMovieTitle()?.[index] ?? '******'
+    }
+
+    return ''
+  }
+
+  const getLetterButtonClass = (letter: string): string => {
+    return currentGuess.includes(letter)
+      ? 'letter-button-disabled'
+      : 'letter-button'
+  }
+
+  const makeGuess = (letter: string) => {
+    if (currentGuess.includes(letter)) return
+    setCurrentGuess([...currentGuess, letter])
+    setTries(tries + 1)
+
+    const correctGuesses: number =
+      currentMovieTitle()
+        ?.split('')
+        .filter((e) => e === letter).length ?? 0
+    setProgress(progress + correctGuesses)
+  }
+
+  useEffect(() => {
+    loadGame()
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='card p-6 relative'>
+      <h2>Guess the movie!</h2>
+      <div className='debug'>{currentMovieTitle()}</div>
+      <div className='flex mt-2 text-lg'>
+        <div className='rounded-sm p-2 bg-purple-100 text-purple-700 mr-2'>
+          <i className='lni lni-calendar mr-1'></i>
+          {new Date(currentMovie?.release_date || '').toLocaleDateString(
+            'en-US',
+            { month: 'short', year: 'numeric' }
+          )}
+        </div>
+        <div className='rounded-sm p-2 bg-green-100 text-green-700'>
+          <i className='lni lni-star-fill mr-1'></i>
+          {currentMovie?.vote_average} stars
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* <div className='progress-circle'>
+        <div className='pc-overlay'>{progressPercent}%</div>
+        <div
+          className='pc-background'
+          style={{
+            background: `conic-gradient(#ec826f ${degrees}deg, #ddd ${degrees}deg)`
+          }}
+        ></div>
+      </div> */}
+
+      <div className='text-teal-600 bg-teal-600/10 py-2 px-6 text-lg absolute top-[6px] right-[6px] rounded-full'>
+        {tries} tries
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='flex flex-wrap justify-center items-center mb-6 sm:mb-8'>
+        {currentMovieTitle()
+          ?.split('')
+          .map((l, i) => {
+            if (l === ' ') return <span className='mx-8'></span>
+            return (
+              <input
+                key={i}
+                readOnly
+                className='outline-0 border-0 border-b-2 w-[3vw] md:w-[40px] xl:w-[60px] h-[3vw] md:h-[40px] lg:h-[60px] mx-2 bg-transparent text-center text-xs md:text-lg text-sky-800'
+                value={getGuessedLetter(i)}
+              />
+            )
+          })}
       </div>
-    </main>
+
+      <div className='button-container'>
+        <div
+          className='absolute w-full h-full'
+          style={{
+            pointerEvents: puzzleSolved() ? 'all' : 'none',
+          }}
+        ></div>
+        {alphabets.map((l) => (
+          <button
+            key={l}
+            className={getLetterButtonClass(l)}
+            title={
+              currentGuess.includes(l) ? `${l} already picked` : `Pick ${l}`
+            }
+            style={{ cursor: currentGuess.includes(l) ? 'default' : 'pointer' }}
+            onClick={() => makeGuess(l)}
+          >
+            {l}
+          </button>
+        ))}
+      </div>
+      {puzzleSolved() && (
+        <div className='my-4 text-center'>
+          Congratulations! You guessed the movie{' '}
+          <code className='text-bold'>{currentMovieTitle()}</code> in {tries}{' '}
+          {triesWord()}! Click on the button below to begin a new game.
+        </div>
+      )}
+      <div className='my-4 flex justify-center items-center'>
+        <button
+          id='new_game'
+          className={
+            'my-4 py-3 px-8 border-0 rounded-sm shadow-lg cursor-pointer text-white text-md flex justify-center items-center'
+          }
+          style={{ background: 'var(--linkColor)' }}
+          onClick={loadGame}
+        >
+          <i className='lni lni-game mr-4' style={{ fontSize: 32 }}></i>
+          New Game
+        </button>
+      </div>
+    </div>
   )
 }
+
+export default WordGame
